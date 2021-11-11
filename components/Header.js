522be73a536b6e2f37a83ffb03a5f05ffab8016b
Aditya Sinha <sinhadityaforever@gmail.com>
2021-11-12 00:26:33 +0530

    Built header-left and header-middle

---
+++ components/Header.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
+function Header() {
+  return (
+    <header
+      className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md
+    p-5 md:px-10"
+    >
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        ></input>
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"></SearchIcon>
+      </div>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;

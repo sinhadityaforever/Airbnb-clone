cd7689d166d7de1d78db7bd0f21df0c0280e89e5
Aditya Sinha <sinhadityaforever@gmail.com>
2021-11-15 20:58:32 +0530

    Minor changes

--- components/Header.js
+++ components/Header.js
@@ -75,7 +75,7 @@ function Header({ placeholder }) {
       </div>
       <div className="flex items-center justify-end space-x-4 text-gray-500">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
-        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <GlobeAltIcon className="hidden md:inline-flex h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
           <MenuIcon className="h-6" />
           <UserCircleIcon className="h-6" />

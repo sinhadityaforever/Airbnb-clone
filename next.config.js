44750d203cc43cd27fbe262baf53b0f5886c2fa0
Aditya Sinha <sinhadityaforever@gmail.com>
2021-11-17 00:56:09 +0530

    Finalizing things-2

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
-  env: {
-    mapbox_key:
-      "pk.eyJ1IjoiYWRpdHlhczE0IiwiYSI6ImNrdzJlZTBwdzA5a20ycHFseDhuN2VoOGMifQ.eKDYw4ZCVecqF4d_P1Ywiw",
-  },
 };
